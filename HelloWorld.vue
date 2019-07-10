34e2287fd4c3d8ae78e9d3af87a71c952d13af70
ashev-pm <44460568+ashev-pm@users.noreply.github.com>
2019-07-10 14:32:44 +0300

    Update

---
+++ HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <v-layout text-xs-center wrap>
+      <v-flex xs12>
+        <v-img 
+        :src="require('../assets/ragio-g-small.png')" 
+        class="my-5" 
+        contain height="420">
+        </v-img>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
---
+++ Info.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container>
+    <v-card class="slider">
+      <v-carousel height="auto" hide-controls="true">
+        <v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src"></v-carousel-item>
+      </v-carousel>
+    </v-card>
+  </v-container>
+</template>
+
+
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          src:
+            "https://raw.githubusercontent.com/ashev-pm/pre-velikolepie/master/sucess.png"
+        },
+        {
+          src:
+            "https://raw.githubusercontent.com/ashev-pm/pre-velikolepie/master/positive.png"
+        },
+        {
+          src:
+            "https://raw.githubusercontent.com/ashev-pm/pre-velikolepie/master/luck.png"
+        },
+        {
+          src:
+            "https://raw.githubusercontent.com/ashev-pm/pre-velikolepie/master/blago.png"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.slider {
+  max-width: 100%;
+  margin-top: -210px;
+  margin-bottom: -84px;
+  position: relative;
+  z-index: 1000;
+}
+</style>
+
