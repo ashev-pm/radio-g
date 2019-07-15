b59a774759f7bb61058fb18699c1dc478c0a965c
TAS-PM <TAS_PM@Administrators-MacBook-Air.local>
2019-07-15 09:40:50 +0300

    Yellow kak mocha

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -4,14 +4,14 @@
       <v-btn
         outline
         icon
-        class="white--text"
+        class="yellow--text"
         @click.native="play"
       >
         <v-icon v-if="!isPlayed">play_arrow</v-icon>
         <v-icon v-else>pause</v-icon>
       </v-btn>
       
-      <v-btn outline icon class="white--text" @click.native="mute()">
+      <v-btn outline icon class="yellow--text" @click.native="mute()">
         <v-icon v-if="!isMuted">volume_up</v-icon>
         <v-icon v-else>volume_off</v-icon>
       </v-btn>
