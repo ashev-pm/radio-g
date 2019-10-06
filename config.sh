1cd9caa917b6637f6cf14495e9d15af6951b3e4f
Alexey Bezborodov <ao003587@gmail.com>
2019-10-06 17:13:03 +0300

    added icecast config, liquidsoap systemctl service, update vagrant script and setup machine script

---
+++ config.sh
@@ -0,0 +1,25 @@
+#docker
+sudo apt update -y
+sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+sudo apt update -y
+sudo apt install docker-ce -y
+
+#nodejs
+sudo apt-get install curl software-properties-common
+curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
+sudo apt install nodejs
+
+#vuejs
+sudo npm install -g @vue/cli
+sudo npm install -g  @vue/cli-service
+
+#liquidsoap
+sudo apt install liquidsoap -y
+sudo cp ~/radio-g/liquidsoap/liquidsoap.service /etc/systemd/system/liquidsoap.service
+sudo cp -r ~/radio-g/liquidsoap/ /usr/share/liquidsoap/
+sudo systemctl start liquidsoapd.service
+
+#icecast
+#sudo apt install icecast2 -y
